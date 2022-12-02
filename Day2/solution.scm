07b7590feab65cecb7d345e65e4e9b84641e3606
Ata Noor <ataulnoor75@gmail.com>
2022-12-02 01:01:05 -0500

    Day 2 - fixed

--- Day2/solution.scm
+++ Day2/solution.scm
@@ -12,64 +12,48 @@
         (let ((enemy-value (list-ref (list-ref TOKENS index) 0)) (user-value (list-ref (list-ref TOKENS index) 1)) (next-index (+ index 1)))
             (cond 
                 ((equal? user-value "X")
-                    (
-                        (cond 
-                            ((equal? enemy-value "A") 
-                                (
-                                    (play-game (+ user-score 3) next-index)
-                                ))
-                            ((equal? enemy-value "B") 
-                                (
-                                    (play-game (+ user-score 1) next-index)
-                                ))
-                            ((equal? enemy-value "C") 
-                                (
-                                    (play-game (+ user-score 2) next-index)
-                                ))
+                    (cond 
+                        ((equal? enemy-value "A") 
+                            (play-game (+ user-score 3) next-index)
+                        )
+                        ((equal? enemy-value "B") 
+                            (play-game (+ user-score 1) next-index)
+                        )
+                        ((equal? enemy-value "C") 
+                            (play-game (+ user-score 2) next-index)
                         )
                     )
                 )
                 ((equal? user-value "Y") 
-                    (
-                        (cond 
-                            ((equal? enemy-value "A") 
-                                (
-                                    (play-game (+ user-score 4) next-index)
-                                ))
-                            ((equal? enemy-value "B") 
-                                (
-                                    (play-game (+ user-score 5) next-index)
-                                ))
-                            ((equal? enemy-value "C") 
-                                (
-                                    (play-game (+ user-score 6) next-index)
-                                ))
+                    (cond 
+                        ((equal? enemy-value "A") 
+                            (play-game (+ user-score 4) next-index)
+                        )
+                        ((equal? enemy-value "B")
+                            (play-game (+ user-score 5) next-index)
+                        )
+                        ((equal? enemy-value "C") 
+                            (play-game (+ user-score 6) next-index)
                         )
                     )
                 )
                 ((equal? user-value "Z") 
-                    (
-                        (cond 
-                            ((equal? enemy-value "A") 
-                                (
-                                    (play-game (+ user-score 8) next-index)
-                                ))
-                            ((equal? enemy-value "B") 
-                                (
-                                    (play-game (+ user-score 9) next-index)
-                                ))
-                            ((equal? enemy-value "C") 
-                                (
-                                    (play-game (+ user-score 7) next-index)
-                                ))
+                    (cond 
+                        ((equal? enemy-value "A") 
+                            (play-game (+ user-score 8) next-index)
+                        )
+                        ((equal? enemy-value "B") 
+                            (play-game (+ user-score 9) next-index)
+                        )
+                        ((equal? enemy-value "C") 
+                            (play-game (+ user-score 7) next-index)
                         )
                     )
                 )
             )
         )
-        (display #\newline)
+        user-score
     )
-    user-score
 )
 (display (play-game 0 0))
 (newline)
